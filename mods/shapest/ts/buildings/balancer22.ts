2a2b0554fc8825a48b20fde1c33d92bd9672d769
TJC games <73426353+TJCgames@users.noreply.github.com>
2022-04-23 11:13:04 +0100

    Rebalance the balancers

--- mods/shapest/ts/buildings/balancer22.ts
+++ mods/shapest/ts/buildings/balancer22.ts
@@ -4,6 +4,8 @@ import { Entity, enumBalancerVariants, enumDirection, enumItemProcessorTypes, fo
 import { RESOURCES } from '../types/common.js';
 
 const var22 = 'two-two-balancer';
+const var31 = 'tri-splitter';
+const var13 = 'tri-merger';
 
 export class Balancer22 {
 	static install(mod: Mod) {
@@ -16,7 +18,55 @@ export class Balancer22 {
 			var22,
 			{
 				name: "2-2 balancer",
-				description: "...The only balancer you ever need",
+				description: "Smaller is better",
+
+				tutorialImageBase64: RESOURCES.splitter1,
+				regularSpriteBase64: RESOURCES.splitter1,
+				blueprintSpriteBase64: RESOURCES.splitter1,
+
+				dimensions: new Vector(1, 1),
+
+				additionalStatistics(root) {
+					const speed = root.hubGoals.getProcessorBaseSpeed(enumItemProcessorTypes.balancer);
+					return [[T.ingame.buildingPlacement.infoTexts.speed, formatItemsPerSecond(speed)]];
+				},
+
+				isUnlocked(root) {
+					return true;
+				},
+			}
+		);
+		mod.modInterface.addVariantToExistingBuilding(
+			// @ts-ignore
+			MetaBalancerBuilding,
+			var31,
+			{
+				name: "3-1 splitter",
+				description: "For when you want to have multiple lines",
+
+				tutorialImageBase64: RESOURCES.splitter1,
+				regularSpriteBase64: RESOURCES.splitter1,
+				blueprintSpriteBase64: RESOURCES.splitter1,
+
+				dimensions: new Vector(1, 1),
+
+				additionalStatistics(root) {
+					const speed = root.hubGoals.getProcessorBaseSpeed(enumItemProcessorTypes.balancer);
+					return [[T.ingame.buildingPlacement.infoTexts.speed, formatItemsPerSecond(speed)]];
+				},
+
+				isUnlocked(root) {
+					return true;
+				},
+			}
+		);
+		mod.modInterface.addVariantToExistingBuilding(
+			// @ts-ignore
+			MetaBalancerBuilding,
+			var13,
+			{
+				name: "1-3 merger",
+				description: "three becomes one",
 
 				tutorialImageBase64: RESOURCES.splitter1,
 				regularSpriteBase64: RESOURCES.splitter1,
@@ -47,12 +97,30 @@ export class Balancer22 {
 						{ pos: new Vector(0, 0), direction: enumDirection.bottom },
 						{ pos: new Vector(0, 0), direction: enumDirection.left },
 					]);
+				} else if (variant === var13) {
+					entity.components.ItemEjector.setSlots([
+						{ pos: new Vector(0, 0), direction: enumDirection.top },
+					]);
+					entity.components.ItemAcceptor.setSlots([
+						{ pos: new Vector(0, 0), direction: enumDirection.bottom },
+						{ pos: new Vector(0, 0), direction: enumDirection.left },
+						{ pos: new Vector(0, 0), direction: enumDirection.right },
+					]);
+				} else if (variant === var31} {
+					entity.components.ItemEjector.setSlots([
+						{ pos: new Vector(0, 0), direction: enumDirection.top },
+						{ pos: new Vector(0, 0), direction: enumDirection.left },
+						{ pos: new Vector(0, 0), direction: enumDirection.right },
+					]);
+					entity.components.ItemAcceptor.setSlots([
+						{ pos: new Vector(0, 0), direction: enumDirection.bottom },
+					]);
 				} else {
 					$old.updateVariants.call(this, entity, rotationVariant, variant);
 				}
 			},
 			getAvailableVariants(root: GameRoot) {
-				return [var22];
+				return [defaultBuildingVariant, var22, var13, var31];
 			},
 		}));
 	}
